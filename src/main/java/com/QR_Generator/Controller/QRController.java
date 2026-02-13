db55af7e513e130782d388f06095d7b7a1e637b2
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-14 01:35:09 +0530

    commit

--- src/main/java/com/QR_Generator/Controller/QRController.java
+++ src/main/java/com/QR_Generator/Controller/QRController.java
@@ -1,14 +1,23 @@
 package com.QR_Generator.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class QRController {
 
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
     @PostMapping("/generate")
-    public String getResponse(){
-        return "index.html";
+    public String QRGenerator(@RequestParam("text") String text, Model model){
+        System.out.println("User Entered : " + text);
+        model.addAttribute("message", "You entered: " + text);
+        return "index";
     }
 }
