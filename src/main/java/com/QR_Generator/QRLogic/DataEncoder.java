c0bc051b17c7d83d50b5d835191a4cc21a45a076
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-15 02:17:58 +0530

    commit

---
+++ src/main/java/com/QR_Generator/QRLogic/DataEncoder.java
@@ -0,0 +1,42 @@
+package com.QR_Generator.QRLogic;
+
+import static java.awt.SystemColor.text;
+
+public class DataEncoder {
+    public String encodeData(String text, EncodingMode mode){
+        int len = text.length();
+        StringBuilder result = new StringBuilder();
+
+        if(mode == EncodingMode.ByteMode){
+            for(int i = 0; i < len; i++){
+                char ch = text.charAt(i);
+                // get ASII
+                int val = (int) ch;
+                // Convert to binary
+                String ans = Integer.toBinaryString(val);
+                // make it 8 bit
+                while(ans.length() < 8){
+                    ans = "0" + ans;
+                }
+                result.append(ans);
+            }
+        }
+        return result.toString();
+    }
+
+
+    public String BitStreamBuilder(String text){
+        ModelSwitcher detector = new ModelSwitcher();
+        EncodingMode mode = detector.DetectionMode(text);
+
+        String modebits = detector.getModeIndicator(mode);
+        String chartobinary = detector.getCharacterCountBinary(text, mode);
+
+        DataEncoder dataEncoder = new DataEncoder();
+        String encode = dataEncoder.encodeData(text, mode);
+
+        String finalbits = modebits + chartobinary + encode;
+        return finalbits;
+    }
+
+}
